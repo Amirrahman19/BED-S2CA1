58351e54e7dba6ac6b71d33dc576cf583304d053
Amirrahman19 <19.amir.rahman@gmail.com>
2021-12-19 16:46:18 +0800

    updated

--- models/Product.js
+++ models/Product.js
@@ -28,26 +28,6 @@ const Product = {
       }
     });
   }, 
-  // findAll: (callback) => {
-  //   var dbConn = db.getConnection();
-  //   dbConn.connect((err) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return callback(err, null);
-  //     } else {
-  //       var sql = "SELECT * FROM users;";
-  //       dbConn.query(sql, (error, results) => {//square for array of all the userids
-  //         dbConn.end();
-  //         if (error) {
-  //           return callback(error, null);
-  //         };
-  //         console.log(results);
-  //         return callback(null, results);
-  //       });
-
-  //     }
-  //   });
-  // },
   insert: (product, callback) => {
     var dbConn = db.getConnection();
     dbConn.connect((err) => {
@@ -66,24 +46,6 @@ const Product = {
       }
     });
   },
-  // edit: (productid, product, callback) => {
-  //   var dbConn = db.getConnection();
-  //   dbConn.connect((err) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return callback(err, null);
-  //     } else {
-  //       var sql = "UPDATE product SET productid = ?, name = ?, description = ?, categoryid = ? , brand = ?, price = ? WHERE productid = ?;";
-  //       dbConn.query(sql, [product.productid, product.name, product.description, product.categoryid, product.brand, product.price, productid], (error, results) => {
-  //         dbConn.end();
-  //         if (error) {
-  //           return callback(error, null);
-  //         };
-  //         return callback(null, results);
-  //       });
-  //     }
-  //   });
-  // },
   delete(productid, callback) {
     var dbConn = db.getConnection();
     dbConn.connect((err) => {
@@ -92,7 +54,7 @@ const Product = {
         return callback(err, null);
       } else {
         var sql = "DELETE FROM product WHERE productid = ?;";
-        dbConn.query(sql, [productid], (error, results) => {//square for array of all the userids
+        dbConn.query(sql, [productid], (error, results) => {
           dbConn.end();
           if (error) {
             return callback(error, null);
