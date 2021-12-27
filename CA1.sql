d00b123560792021fba35ad3691f812ca94f7272
Amirrahman19 <19.amir.rahman@gmail.com>
2021-12-27 16:45:15 +0800

    updated

--- CA1.sql
+++ CA1.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sp_it` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sp_it`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sp_it
@@ -197,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-27 16:42:13
+-- Dump completed on 2021-12-27 16:44:52
